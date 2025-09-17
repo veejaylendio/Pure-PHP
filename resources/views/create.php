bcb4a0a7175f77f200c209885359a9d447ac4aef
veejaylendio <lendioveejay@gmail.com>
2025-09-17 22:57:08 +0800

    Created asso. array that will store the value from the form. And the next move is to store the array in .json file

--- resources/views/create.php
+++ resources/views/create.php
@@ -1,11 +1,17 @@
 <?php
-$listitems = [];
-if (isset($_POST['items'])) {
-    $items = $_POST['items'];
+session_start();
+//$_SESSION['items'] = [];
 
-    
+if (isset($_POST['submit'])) {
+   foreach ($_POST['items'] as $item) {
+       $_SESSION['items'][] = $item;
+   }
+    $json = file_get_contents('data.json');
+    $data = json_decode($json);
+    $data[] = $_POST['data'];
+    file_put_contents('data.json', json_encode($data));
 }
-var_dump($_POST['items']);
+var_dump($_SESSION['items']);
 ?>
 
 <!doctype html>
@@ -27,7 +33,7 @@ var_dump($_POST['items']);
             <form method="post" action="create.php">
                 <label for="fname">New Item:</label><br>
                 <input type="text" id="input-new-item" name="items[]" placeholder="Add New Item"><br>
-                <input type="submit" class="button btn-add-list" value="Add">
+                <input type="submit" name="submit" class="button btn-add-list" value="Add">
 <!--                <input type="submit" id="cancel" class="button btn-delete-item" value="Cancel">-->
             </form>
         </div>
